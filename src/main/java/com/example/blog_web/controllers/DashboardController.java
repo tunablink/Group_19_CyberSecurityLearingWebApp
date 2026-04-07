9a5d073e30aff7482fc4068ca0a0cbc2bd5611e7
daichit <gene.sixteen.000925@outlook.jp>
2026-04-07 23:23:53 +0700

    add current user infor dashboardControlloer /dashboard

--- src/main/java/com/example/blog_web/controllers/DashboardController.java
+++ src/main/java/com/example/blog_web/controllers/DashboardController.java
@@ -1,13 +1,29 @@
 package com.example.blog_web.controllers;
 
+import com.example.blog_web.models.User;
+import com.example.blog_web.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class DashboardController {
 
+    private final UserService userService;
+
+    public DashboardController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal) {
+        if (principal != null) {
+            String username = principal.getName();
+            User user = userService.findByUsername(username);
+            model.addAttribute("user", user);
+        }
         return "dashboard/dashboard";
     }
 }
